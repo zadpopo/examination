f5783130e68cf6303567aa2505a799a60eb77dd4
zadpopo <39912714+zadpopo@users.noreply.github.com>
2019-04-15 16:15:20 +0800

    Add files via upload

--- emp/examact.php
+++ emp/examact.php
@@ -5,6 +5,19 @@ include "php/connections.php";
 	if ($_GET) {
 		$id = $_GET['id'];
 
+
+		$check_t= mysqli_query($conn, "SELECT * FROM timetbl WHERE exam_id='$id'");
+ 		$rows_ct= mysqli_num_rows($check_t);
+ 		if($rows_ct > 0) {
+
+
+
+		$check_q= mysqli_query($conn, "SELECT * FROM examtbl WHERE exam_no='$id'");
+ 		$rows_cq= mysqli_num_rows($check_q);
+ 		if($rows_cq > 0) {
+
+		
+
 		$sql ="UPDATE lexamtb SET status = '1' WHERE exam_no ='$id'";
 
 
@@ -19,7 +32,21 @@ include "php/connections.php";
 			echo "Erorr updating record: " .$conn->error;
 		}
 			$conn->close();
+
+	}else{
+
+		echo "<script language = 'javascript'>alert('Failed! The Exam doesn not have any question !')</script>";
+		echo "<script>window.location.href = 'exam_list.php';</script>";
+
 	}
 
+}else{
+
+	echo "<script language = 'javascript'>alert('Failed! The Exam doesn not have timer !')</script>";
+		echo "<script>window.location.href = 'exam_list.php';</script>";
+
+
+}
+}
 
 ?>
